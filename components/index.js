17e317d12503db6c761b08ba210b69d518dc10e7
Santiago Agudelo <sagudelo1200@gmail.com>
2025-10-27 14:30:12 -0500

    Limpieza de codigo sin uso

--- components/index.js
+++ components/index.js
@@ -1,7 +1,6 @@
 import Button from './Button';
 import Icon from './Icon';
-import Header from './Header';
 import Input from './Input';
 import LoadingScreen from './LoadingScreen';
 
-export { Button, Icon, Input, Header, LoadingScreen };
+export { Button, Icon, Input, LoadingScreen };
--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,4 @@
 import argonTheme from './Theme';
-import tabs from './tabs';
 import utils from './utils';
 
-export { argonTheme, tabs, utils };
+export { argonTheme, utils };
